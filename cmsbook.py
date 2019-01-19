abf7d4c6c57c681f39ab9150d4bf947e2fac6432
Tai Sakuma <tai.sakuma@gmail.com>
2019-01-19 10:30:49 +0000

    make pagemenutitle, pagemenu parameters

--- cmsbook.py
+++ cmsbook.py
@@ -10,6 +10,39 @@ def index():
     subheadernavi = ''.join(make_subhead_navi(topcontents, ''))
     return render_template('index.html', subheadernavi=subheadernavi)
 
+@app.route('/<path:path>')
+def page(path):
+    path_items = path.split('/')
+    parentdir = path_items[0]
+    subheadernavi = ''.join(make_subhead_navi(topcontents, parentdir=parentdir))
+    pagemenutitle = '<a href="../../references/s0000_index_001" class="selected">References</a>'
+    return render_template(
+        'page.html',
+        subheadernavi=subheadernavi,
+        pagemenutitle=pagemenutitle,
+        pagemenu=pagemenu
+    )
+
+pagemenu = """
+        <ul>
+            <li class=""><div><a href="../inbox/md.php?md=web.md">&nbsp;&nbsp;inbox (unsorted)</a></div></li>
+            <li class=""><div><a href="../CMS/md.php?md=web.md">&nbsp;&nbsp;CMS</a></div></li>
+            <li class=""><div><a href="../ATLAS/md.php?md=web.md">&nbsp;&nbsp;ATLAS</a></div></li>
+            <li class=""><div><a href="../LHC/md.php?md=web.md">&nbsp;&nbsp;LHC</a></div></li>
+            <li class=""><div><a href="../PDG/md.php?md=web.md">&nbsp;&nbsp;PDG</a></div></li>
+
+            <li class=" has_subcontents"><div><a href="../statistics/md.php?md=web.md">&nbsp;&nbsp;Statistics</a></div><ul>
+                <li class=""><div><a href="../statistics/md.php?md=asimov.md">&nbsp;&nbsp;Asimov</a></div></li>
+            </ul>
+            </li>
+            <li class=""><div><a href="../LPCC/md.php?md=web.md">&nbsp;&nbsp;LPCC LHC WGs</a></div></li>
+            <li class=""><div><a href="../LHCHXSWG/md.php?md=web.md">&nbsp;&nbsp;LHC Higgs Xsec WG</a></div></li>
+            <li class=""><div><a href="../SUSY/md.php?md=web.md">&nbsp;&nbsp;SUSY</a></div></li>
+            <li class=""><div><a href="../DarkMatter/md.php?md=web.md">&nbsp;&nbsp;Dark Matter</a></div></li>
+            <li class=" selected"><div><a href="../MachineLearning/md.php?md=web.md" class="selected">&nbsp;&nbsp;Machine Learning</a></div></li>
+        </ul>
+"""
+
 topcontents = [
 	dict(head='Conferences', localonly=True, parentdir='conferences', href='../../conferences/s0000_index_001'),
 	dict(separator=True),
