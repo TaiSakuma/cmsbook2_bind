15d10b230c9ce228d54faa3e98f3a1c8c354da9d
Tai Sakuma <tai.sakuma@gmail.com>
2019-01-19 22:17:03 +0000

    add copy contents in _copy_contents()

--- cmsbook2/pagemenu.py
+++ cmsbook2/pagemenu.py
@@ -7,13 +7,28 @@ def make_pagemenu(contents, thisdir, thisfile):
 
 ##__________________________________________________________________||
 def _expand_contents(contents, thisdir, thisfile):
-    contents = _add_href(contents)
+    contents = _copy_contents(contents)
+    _add_href(contents)
+    _add_thisfile(contents, thisdir, thisfile)
+    _add_thisfile_ancestor(contents)
+    return contents
+
+def _copy_contents(contents):
+    """return a copy of contents
+
+    This function is used instead of the very slow copy.deepcopy().
+    """
+    ret = [i.copy() for i in contents]
+    for item in ret:
+        if 'subcontents' in item:
+            item['subcontents'] = _copy_contents(item['subcontents'])
+    return ret
 
 def _add_href(contents):
-    return [_add_href_item(i) for i in contents]
+    for item in contents:
+        _add_href_item(item)
 
 def _add_href_item(item):
-    item = item.copy()
     if 'href' not in item:
         if 'dir' in item:
             if 'file' in item:
@@ -24,8 +39,29 @@ def _add_href_item(item):
                 item['href'] = '../{}'.format(item['file'])
 
     if 'subcontents' in item:
-        item['subcontents'] = _add_href(item['subcontents'])
-    return item
+        _add_href(item['subcontents'])
+
+def _add_thisfile(contents, thisdir, thisfile):
+    for item in contents:
+        _add_thisfile_item(item, thisdir, thisfile)
+
+def _add_thisfile_item(item, thisdir, thisfile):
+    if 'dir' in item and 'file' in item:
+        if item['dir'] == thisdir and item['file'] == thisfile:
+            item['thisfile'] = True
+
+    if 'subcontents' in item:
+        _add_thisfile(item['subcontents'], thisdir, thisfile)
+
+def _add_thisfile_ancestor(contents):
+    for item in contents:
+        if item.get('thisfile', False):
+            return True
+        if 'subcontents' in item:
+            if _add_thisfile_ancestor(item['subcontents']):
+                item['thisfile_ancestor'] = True
+                return True
+    return False
 
 ##__________________________________________________________________||
 def _render_contents(contents):
--- tests/test_pagemenu.py
+++ tests/test_pagemenu.py
@@ -2,7 +2,10 @@
 import pytest
 
 from cmsbook2.pagemenu import make_pagemenu, _render_item
+from cmsbook2.pagemenu import _copy_contents
 from cmsbook2.pagemenu import _add_href_item
+from cmsbook2.pagemenu import _add_thisfile_item
+from cmsbook2.pagemenu import _add_thisfile_ancestor
 
 ##__________________________________________________________________||
 params = [
@@ -81,6 +84,33 @@ def test_render_item(item, expected):
 
 ##__________________________________________________________________||
 params = [
+    pytest.param([dict(head="AAA")], id='simple'),
+    pytest.param(
+        [dict(head="AAA", subcontents=[dict(head="BBB")])],
+        id='nested-1'),
+    pytest.param(
+        [dict(head="AAA", subcontents=[
+            dict(head="BBB", subcontents=[dict(head="CCC")])
+        ]
+        )],
+        id='nested-2'),
+]
+
+@pytest.mark.parametrize('contents', params)
+def test_copy_contents(contents):
+    copy = _copy_contents(contents)
+    assert copy is not contents
+    for item_copy, item_original in zip(copy, contents):
+        assert item_copy is not item_original
+        if 'subcontents' in item_original:
+            assert item_copy['subcontents'] is not item_original['subcontents']
+            for item_copy2, item_original2 in zip(item_copy['subcontents'], item_original['subcontents']):
+                assert item_copy2 is not item_original2
+                if 'subcontents' in item_original2:
+                    assert item_copy2['subcontents'] is not item_original2['subcontents']
+
+##__________________________________________________________________||
+params = [
     pytest.param(
         dict(head="AAA", dir='dirA', file='fileA'),
         dict(head="AAA", dir='dirA', file='fileA', href='../dirA/fileA'),
@@ -109,9 +139,8 @@ params = [
 
 @pytest.mark.parametrize('item, expected', params)
 def test_add_href_item(item, expected):
-    actual = _add_href_item(item)
-    assert expected == actual
-    assert actual is not item
+    _add_href_item(item)
+    assert expected == item
 
 params = [
     pytest.param(
@@ -148,9 +177,79 @@ params = [
 
 @pytest.mark.parametrize('item, expected', params)
 def test_add_href_item_subcontents(item, expected):
-    actual = _add_href_item(item)
-    assert expected == actual
-    assert actual is not item
-    assert actual['subcontents'] is not item['subcontents']
+    _add_href_item(item)
+    assert expected == item
+
+##__________________________________________________________________||
+params = [
+    pytest.param(
+        dict(head="AAA", dir='dirA', file='fileA'),
+        'dirA', 'fileA',
+        dict(head="AAA", dir='dirA', file='fileA', thisfile=True),
+        id='simple-true'),
+    pytest.param(
+        dict(head="AAA", dir='dirA', file='fileA'),
+        'dirA', 'fileB',
+        dict(head="AAA", dir='dirA', file='fileA'),
+        id='simple-false-1'),
+    pytest.param(
+        dict(head="AAA", dir='dirA', file='fileA'),
+        'dirB', 'fileA',
+        dict(head="AAA", dir='dirA', file='fileA'),
+        id='simple-false-2'),
+    pytest.param(
+        dict(head="AAA", subcontents=[
+            dict(head="111", dir='dir1', file='file1'),
+            dict(head="222", dir='dir1', file='file2'),
+            dict(head="aaaa", dir='dir2', file='file3', subcontents=[
+                dict(head="333", dir='dir3', file='file4'),
+            ]),
+        ]),
+        'dir3', 'file4',
+        dict(head="AAA", subcontents=[
+            dict(head="111", dir='dir1', file='file1'),
+            dict(head="222", dir='dir1', file='file2'),
+            dict(head="aaaa", dir='dir2', file='file3', subcontents=[
+                dict(head="333", dir='dir3', file='file4', thisfile=True),
+            ]),
+        ]),
+        id='nested'),
+]
+
+@pytest.mark.parametrize('item, thisdir, thisfile, expected', params)
+def test_add_thisfile_item(item, thisdir, thisfile, expected):
+    _add_thisfile_item(item, thisdir, thisfile)
+    assert expected == item
+
+##__________________________________________________________________||
+params = [
+    pytest.param(
+        [
+            dict(head="AAA", subcontents=[
+                dict(head="111", dir='dirA', file='file111'),
+                dict(head="222", dir='dirA', file='file222'),
+                dict(head="333", dir='dirA', file='file333', subcontents=[
+                    dict(head="333 a", dir='dirA', file='file333_a', thisfile=True),
+                ]),
+            ]),
+            dict(head="BBB", dir='dirA', file='file111')
+        ],
+        [
+            dict(head="AAA", thisfile_ancestor=True, subcontents=[
+                dict(head="111", dir='dirA', file='file111'),
+                dict(head="222", dir='dirA', file='file222'),
+                dict(head="333", dir='dirA', file='file333', thisfile_ancestor=True, subcontents=[
+                    dict(head="333 a", dir='dirA', file='file333_a', thisfile=True),
+                ]),
+            ]),
+            dict(head="BBB", dir='dirA', file='file111')
+        ],
+    ),
+]
+
+@pytest.mark.parametrize('contents, expected', params)
+def test_add_thisfile_ancestor(contents, expected):
+    _add_thisfile_ancestor(contents)
+    assert expected == contents
 
 ##__________________________________________________________________||
