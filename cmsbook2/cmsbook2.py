6e14bf90401fec2a4b1761afc192b880581fdc75
Tai Sakuma <tai.sakuma@gmail.com>
2019-01-26 16:36:14 +0000

    move sections out of the package

--- cmsbook2/cmsbook2.py
+++ cmsbook2/cmsbook2.py
@@ -14,6 +14,13 @@ def load_chapter_lists():
     spec.loader.exec_module(chapters)
     return chapters.chapters
 
+def load_section_lists():
+    path = '/Users/sakuma/Dropbox/cmsbook/references/cmsbook2_chapter/sections.py'
+    spec = importlib.util.spec_from_file_location('sections', path)
+    sections = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(sections)
+    return sections.sections
+
 @app.route('/')
 def index():
     chapters = load_chapter_lists()
@@ -28,31 +35,14 @@ def page(path):
     subheadernavi = ''.join(make_subhead_navi(chapters, parentdir=parentdir))
     pagemenutitle = '<a href="../../references/s0000_index_001" class="selected">References</a>'
     thisfile = ''
-    pagemenu = ''.join(make_pagemenu(contents, parentdir, thisfile))
+    sections = load_section_lists()
+    pagemenu = ''.join(make_pagemenu(sections, parentdir, thisfile))
     return render_template(
         'page.html',
         subheadernavi=subheadernavi,
         pagemenutitle=pagemenutitle,
         pagemenu=pagemenu
     )
-
-contents = [
-    dict(head="inbox (unsorted)", dir='inbox', file='md.php?md=web.md', lock=False),
-    dict(head="CMS", dir='CMS', file='md.php?md=web.md', lock=False),
-    dict(head="ATLAS", dir='ATLAS', file='md.php?md=web.md', lock=False),
-    dict(head="LHC", dir='LHC', file='md.php?md=web.md', lock=False),
-    dict(head="PDG", dir='PDG', file='md.php?md=web.md', lock=False),
-    dict(head="Statistics", dir='statistics', file='md.php?md=web.md', lock=False,
-         subcontents=[
-             dict(head="Asimov", dir='statistics', file='md.php?md=asimov.md', lock=False),
-         ]
-    ),
-    dict(head="LPCC LHC WGs", dir='LPCC', file='md.php?md=web.md', lock=False),
-    dict(head="LHC Higgs Xsec WG", dir='LHCHXSWG', file='md.php?md=web.md', lock=False),
-    dict(head="SUSY", dir='SUSY', file='md.php?md=web.md', lock=False),
-    dict(head="Dark Matter", dir='DarkMatter', file='md.php?md=web.md', lock=False),
-    dict(head="Machine Learning", dir='MachineLearning', file='md.php?md=web.md', lock=False),
-]
 
 def make_subhead_navi(chapters, parentdir):
     ret = [ ]
